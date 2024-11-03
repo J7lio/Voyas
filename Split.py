c534a92049fdd81b8248963e1515d677e9f74525
Carlos <xcarlos.1003@gmail.com>
2024-11-04 00:19:43 +0100

    Creo la clase Split básica

---
+++ Split.py
@@ -0,0 +1,10 @@
+class Split:
+
+    """
+    name = Nombre del split
+    final_time = Tiempo final al pasar al siguiente split
+    """
+    def __init__(self, name, final_time = None):
+        self.name = name
+        self.final_time = final_time
+
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 import time
+from Split import Split
 
 
-def trans_time(t):
+def transform_time(t):
     mil = int((t - int(t)) * 100)
     sec = int(t % 60)
     minutes = int((t // 60) % 60)
@@ -14,7 +15,7 @@ def trans_time(t):
 def actualizar_timer():
     if running:
         t = time.time() - start_time
-        timer.config(text=trans_time(t))
+        timer.config(text=transform_time(t))
         root.after(10, actualizar_timer)
 
 
@@ -30,13 +31,15 @@ def stop_timer():
     global running
     running = False
 
+def create_split(name):
+    return Split(name)
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("500x500")
     root.title("Voyas")
 
-    start_time = time.time()
+    start_time = 0
     running = False
 
     boton_start = tk.Button(root, text="Start", command=start_timer)
@@ -45,7 +48,7 @@ if __name__ == "__main__":
     boton_stop = tk.Button(root, text="Stop", command=stop_timer)
     boton_stop.pack()
 
-    timer = tk.Label(root, text = trans_time(time.time()), font=("Helvetica", 24))
+    timer = tk.Label(root, text = transform_time(0), font=("Helvetica", 24))
     timer.pack()
 
     root.mainloop()
